9a8ebe41f70ee6d3231f9e4a761408a1cd5449ac
jrfep <jr.ferrer.paris@gmail.com>
2023-04-22 11:07:23 +1000

    explore RS for one model

--- inc/R/03-calc-RS-per-model.R
+++ inc/R/03-calc-RS-per-model.R
@@ -11,8 +11,9 @@
 
 ## Set up / Libraries -------
 
-require(dplyr)
-require(raster)
+library(dplyr)
+library(raster)
+library(dismo)
 
 ## Programing environment variables
 env_file_path <- "proyectos/Tropical-Glaciers/T6.1-tropical-glaciers-suitability-model/"
@@ -25,9 +26,40 @@ source(
 )
 input.dir <- sprintf("%s%s/OUTPUT/",gis.out,projectname)
 
-dir(sprintf("%s/Ecuador/",input.dir))
+dir(sprintf("%s/Mexico/",input.dir))
 
-(load(sprintf("%s/Ecuador/gbm-model-current.rda",input.dir)))
-IV <- predictions[,"G"]
-(load(sprintf("%s/Ecuador/gbm-prediction-2011-2040-mri-esm2-0-ssp126.rda",input.dir)))
-FV <- predictions
+load(sprintf("%s/Mexico/gbm-model-current.rda",input.dir))
+
+all_results <- tibble()
+for (timeframe in c("2011-2040", "2041-2070", "2071-2100")) {
+    for (pathway in c("ssp126")) {
+        for (modelname in "mri-esm2-0") {
+            future_prediction <- readRDS(
+                sprintf("%s/Mexico/gbm-prediction-%s-%s-%s.rds",
+                    input.dir,
+                    timeframe,
+                    modelname,
+                    pathway))
+
+            rslts <- testing %>% 
+                inner_join(future_prediction, by=c("id", "cellnr", "glacier")) %>% 
+                dplyr::select(id, cellnr, glacier, IV, FV) %>% 
+                mutate(timeframe, modelname, pathway)
+            all_results <- all_results %>% bind_rows(rslts)
+
+        }
+    }
+}
+
+CT <- threshold(e1)[c("prevalence","spec_sens","equal_sens_spec")]
+
+RS_results <- all_results %>% 
+    mutate(
+        OD=IV-FV,
+        MD_p=IV-CT[[1]],
+        MD_ss=IV-CT[[2]],
+        MD_ess=IV-CT[[3]],
+        RS_p=OD/MD_p,
+        RS_ss=OD/MD_ss,
+        RS_ess=OD/MD_ess,
+        )
