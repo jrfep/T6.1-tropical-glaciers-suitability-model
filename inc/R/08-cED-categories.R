9cfd220222e9ca2f6486254b2fa0a4b8b1111ec0
jrfep <jr.ferrer.paris@gmail.com>
2023-05-09 08:46:45 +1000

    add plots for collapse trajectories

---
+++ inc/R/08-cED-categories.R
@@ -0,0 +1,116 @@
+library(units)
+library(dplyr)
+library(stringr)
+library(readr)
+library(doParallel)
+
+here::i_am("inc/R/07-glmm-data-summarised.R")
+
+source(here::here("inc","R","RS-functions.R"))
+source(here::here("env","project-env.R"))
+
+if (exists("gis.out")) {
+    target.dir <- sprintf("%s/%s/", gis.out, projectname)
+    rds.file <- sprintf("%s/massbalance-model-data-all-groups.rds", target.dir)
+    out.file <- sprintf("%s/collapse-trajectory-data.rds", target.dir)
+
+} else {
+    target.dir <- "sandbox"
+    rds.file <- here::here(target.dir, "massbalance-model-data-all-groups.rds")
+    out.file <- here::here(target.dir, "collapse-trajectory-data.rds")
+}
+massbalance_results <- readRDS(rds.file)
+
+jj <- "Cordillera de Merida"
+ss <- "SSP5-8.5"
+
+
+cl <- makeCluster(round(detectCores()*.8))
+registerDoParallel(cl)
+
+# create a grid of arguments to avoid for loops within the `ex` code
+jjs <- c("Puncak Jaya", "Kilimanjaro", "Mount Kenia",
+  "Ruwenzori", "Mexico", "Sierra Nevada de Santa Marta",
+  "Cordillera de Merida")
+scs <-  c("SSP1-2.6", "SSP2-4.5", "SSP3-7.0", "SSP5-8.5")
+
+argrid <- expand.grid(jjs,scs)
+
+collapse_trajectories <- foreach (
+    jj = argrid$Var1,
+    ss = argrid$Var2,
+    .packages=c( "dplyr", "tidyr"),
+    .combine=bind_rows,
+    .multicombine = TRUE,
+    .export=c("RSts","summary_cED_w", "cED_w")
+  ) %dopar% {
+    mbdata <- massbalance_results %>% 
+    filter(
+        unit_name %in% jj,
+        ssp %in% ss
+        ) 
+
+    RSa_data <- mbdata %>%
+        group_by(model_nr,year) %>%
+        summarise(total_mass=sum(mass), .groups="keep") %>%
+        ungroup %>%
+        group_by(model_nr) %>%
+        group_modify(
+            ~RSts(
+                .x$year,
+                .x$total_mass,
+                formula = "conditional"))
+
+    wgs <- mbdata %>% 
+        filter(year == 2000) %>% 
+        group_by( RGIId, model_nr) %>% 
+        summarise(initial_mass=sum(mass), .groups="keep") 
+
+    RSi_data <- mbdata %>% 
+    group_by(RGIId, model_nr) %>% 
+    group_modify(~RSts(.x$year,.x$mass,
+                        formula = "conditional")) %>%
+    ungroup %>% 
+    left_join(wgs, by = c("RGIId", "model_nr"))
+    
+    cED_data <- RSi_data %>%
+        group_by(model_nr,year) %>% 
+        #group_map(~cED_w(.x$RS,.x$initial_mass))
+        group_modify(~summary_cED_w(.x$RS,.x$initial_mass)) %>%
+        ungroup 
+
+    collapse_year <- RSa_data %>% filter(RS==1) %>% summarise(collapse_year=min(year)) 
+
+    all_data <- 
+    RSa_data %>% 
+    inner_join(cED_data,by=c("model_nr","year")) %>% 
+    left_join(collapse_year, by="model_nr") %>%
+    mutate(
+        unit_name = jj,
+        scenario = ss,
+        state = case_when(
+            RS == 1 ~ "collapsed",
+            cED_80>=0.80 ~ "very wide",
+            cED_80>=0.50 ~ "very inter",
+            cED_50>=0.80 ~ "high wide",
+            cED_80>=0.30 ~ "very local",
+            cED_50>=0.50 ~ "high inter",
+            cED_30>=0.80 ~ "mod wide",
+            TRUE ~ "low"
+        ),
+        countdown = if_else(is.na(collapse_year), 2100 - year, collapse_year - year)
+    ) %>%
+        select(unit_name, scenario, year, countdown, model_nr, RS:AUC_cED, state) 
+
+    return(all_data)
+  }
+
+
+## Stop cluster: garbage collection ----
+
+stopCluster(cl)
+gc()
+
+## Output: save data to Rdata file ----
+
+saveRDS(file=out.file, collapse_trajectories)
--- inc/R/RS-shared.R
+++ inc/R/RS-shared.R
@@ -17,8 +17,19 @@ unit_order <- c(
 
 okabe <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 IUCN_cat_colours <- c("LC"="darkgreen", "VU"="yellow", "EN"="orange", "CR"="red", "CO"="black")
- 
 IUCN_cat_okabe <- c("LC"=okabe[3], "VU"=okabe[4], "EN"=okabe[1], "CR"=okabe[6], "CO"=okabe[7])
+
+state_cat_okabe <- c(
+  "low"=okabe[5], 
+  "mod wide"=okabe[2], 
+  "high inter"=okabe[3],
+  "very local"=okabe[4], 
+  "high wide"=okabe[1],
+  "very inter"=okabe[6],
+  "very wide"=okabe[7], 
+  "collapsed"="black")
+
+state_order <- names(state_cat_okabe)
 
 #| ggplot theme
 old <- theme_set(theme_linedraw())
